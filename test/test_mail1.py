8f3db97c5e803a2c3a6bb42561d4321581a9cccb
Yurii <116266510+xtxthby@users.noreply.github.com>
2024-06-29 22:11:45 +0300

    add file

---
+++ test/test_mail1.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.fixture()#дія до тог як ми запутимо систему
+
+def set_up():
+    print('Вхід в систему')
+
+def test_sending_mail1(set_up):# передаємо функцію для входа всистему
+    print('Лист відправлено')
+
+def test_sending_mail2(set_up):# передаємо функцію для входа всистему
+    print('Лист відправлено')
---
+++ test/test_mail2.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.fixture()#дія до тог як ми запутимо систему
+
+def set_up():
+    print('Вхід в систему')
+
+def test_sending_mail1():# передаємо функцію для входа всистему
+    print('Лист відправлено')
+
+def test_sending_mail2():# передаємо функцію для входа всистему
+    print('Лист відправлено')
